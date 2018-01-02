79464e5524dc600b3fc9604ac1f82d8f1a7a53b4
Daniel Baena Hermosa <daniel.baena@machiina.com>
2018-01-02 13:11:49 +0100

    Add sketch resource and images

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -1,8 +1,8 @@
 export default {
-    white: '#ffffff',
-    black: '#282629',
-    softgrey: '#F2F2F2',
-    grey: '#9B9B9B',
-    red: '#DD382C',
-    disabled: '#F7D1CE',
+    white    : '#ffffff',
+    black    : '#282629',
+    softgrey : '#F2F2F2',
+    grey     : '#9B9B9B',
+    red      : '#DD382C',
+    disabled : '#F7D1CE',
 }
